783a75a20ab619b5b4b7b8b359e71fef6e872717
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-02-15 16:36:28 +0300

    Add project with Autofac module, which registering implementations for IDbExecutor and IDbExecutorFactory

---
+++ DbConn.DbExecutor.Dapper.Ioc.Autofac/DbExecutorRegistrationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+using DbConn.DbExecutor.Abstract;
+
+namespace DbConn.DbExecutor.Dapper.Ioc.Autofac
+{
+    public class DbExecutorRegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<DapperDbExecutorFactory>()
+                .As<IDbExecutorFactory>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<DapperDbExecutor>()
+                .As<IDbExecutor>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
