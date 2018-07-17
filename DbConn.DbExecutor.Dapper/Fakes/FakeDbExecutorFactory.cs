01c0921a3bffa817f18e15c1284a4b0d9faec4f9
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-07-17 12:28:32 +0300

    Add FakeDbExecutorFactory - may be useful for integration tests

---
+++ DbConn.DbExecutor.Dapper/Fakes/FakeDbExecutorFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using DbConn.DbExecutor.Abstract;
+
+namespace DbConn.DbExecutor.Dapper.Fakes
+{
+    public class FakeDbExecutorFactory : IDbExecutorFactory
+    {
+        private readonly string _connectionString;
+
+        public FakeDbExecutorFactory()
+        {
+
+        }
+        public FakeDbExecutorFactory(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentNullException(nameof(connectionString));
+            _connectionString = connectionString;
+        }
+
+        public IDbExecutor Create(string connectionString)
+        {
+            return string.IsNullOrWhiteSpace(_connectionString)
+                ? new DapperDbExecutorWithoutCommit(connectionString)
+                : new DapperDbExecutorWithoutCommit(_connectionString);
+        }
+
+        public IDbExecutor CreateTransactional(string connectionString, IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+        {
+            return string.IsNullOrWhiteSpace(_connectionString)
+                ? new DapperDbExecutorWithoutCommit(connectionString, isolationLevel)
+                : new DapperDbExecutorWithoutCommit(_connectionString, isolationLevel);
+        }
+    }
+}
