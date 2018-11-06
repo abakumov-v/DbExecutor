5c9d6f1c41bcf6ac4f650593c460bfc59527887d
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2018-11-06 11:30:00 +0300

    Add new project to support SQLite connections

---
+++ DbConn.DbExecutor.Dapper.Sqlite/DapperDbExecutorFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using DbConn.DbExecutor.Abstract;
+
+namespace DbConn.DbExecutor.Dapper.Sqlite
+{
+    public class DapperDbExecutorFactory : IDbExecutorFactory
+    {
+        /// <summary>
+        /// Create DbExecutor instance without starting the transaction
+        /// </summary>
+        /// <param name="connectionString">Database connection string</param>
+        /// <returns></returns>
+        public IDbExecutor Create(string connectionString)
+        {
+            return new DapperDbExecutor(connectionString, isNeedTransactional: false);
+        }
+        /// <summary>
+        /// Create DbExecutor instance with the start of transaction
+        /// </summary>
+        /// <param name="connectionString">Database connection string</param>
+        /// <param name="isolationLevel"></param>
+        /// <returns></returns>
+        public IDbExecutor CreateTransactional(string connectionString,
+            IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+        {
+            return new DapperDbExecutor(connectionString, isolationLevel);
+        }
+    }
+}
---
+++ DbConn.DbExecutor.Dapper.Sqlite/DapperDbExecutorWithoutCommit.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace DbConn.DbExecutor.Dapper.Sqlite
+{
+    /// <summary>
+    /// Dapper DB-executor without real commit transaction
+    /// </summary>
+    public class DapperDbExecutorWithoutCommit : DapperDbExecutor
+    {
+        public DapperDbExecutorWithoutCommit(string connectionString,
+            IsolationLevel isolationLevel = IsolationLevel.ReadCommitted, bool isNeedTransactional = true) : base(
+            connectionString, isolationLevel, isNeedTransactional)
+        {
+        }
+
+        /// <summary>
+        /// WARNING: This method will not make a real commit. 
+        /// Instead, it will always roll back. This can be useful in some scenarios, such as 
+        /// integration tests, because we will not commit changes made by integration tests.
+        /// </summary>
+        public override void Commit()
+        {
+            // ignore commit and always rollback transaction
+            Rollback();
+        }
+    }
+}
