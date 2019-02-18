e99061bfabee4228231e8fb5fd31861b1c58b0b0
Valeriy1991 <6594815+Valeriy1991@users.noreply.github.com>
2019-02-18 15:48:18 +0300

    Add autofac registration modules for all implementations of IDbExecutor and IDbExecutorFactory

---
+++ DbConn.DbExecutor.Dapper.Ioc.Autofac/SqlServerDbExecutorRegistrationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Autofac;
+using DbConn.DbExecutor.Abstract;
+
+namespace DbConn.DbExecutor.Dapper.Ioc.Autofac
+{
+    public class SqlServerDbExecutorRegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SqlServer.DapperDbExecutorFactory>()
+                .As<IDbExecutorFactory>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<SqlServer.DapperDbExecutor>()
+                .As<IDbExecutor>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ DbConn.DbExecutor.Dapper.Ioc.Autofac/SqliteDbExecutorRegistrationModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Autofac;
+using DbConn.DbExecutor.Abstract;
+using DbConn.DbExecutor.Dapper.SqlServer;
+
+namespace DbConn.DbExecutor.Dapper.Ioc.Autofac
+{
+    public class SqliteDbExecutorRegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Sqlite.DapperDbExecutorFactory>()
+                .As<IDbExecutorFactory>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<Sqlite.DapperDbExecutor>()
+                .As<IDbExecutor>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
